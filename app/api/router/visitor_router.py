5824b5e7b1c9a58ab47298202b79ed60f3fb39fa
Yuanhao Min <min.yuanhao.1997.11.12@outlook.com>
2023-08-28 08:58:42 +0200

    delete unused code block and optimize requirements.txt

--- app/api/router/visitor_router.py
+++ app/api/router/visitor_router.py
@@ -20,37 +20,6 @@ router = APIRouter(
 logging.basicConfig(level=logging.INFO)
 
 
-# @router.post("/registration/function-call")
-# def determine_registration_function_call(request: DetermineFunctionCallRequest):
-#     result_str = visitor_logic.determine_registration_function_call(request.text)
-#
-#     try:
-#         result = json.loads(result_str)
-#     except json.JSONDecodeError:
-#         raise InvalidAIGeneratedJSONError(detail="Failed to parse JSON from function result.")
-#
-#     valid, errors = validate_json(result, visitor_register_schema)
-#     if not valid:
-#         raise InvalidAIGeneratedJSONError(detail=", ".join(errors))
-#
-#     return result
-#
-#
-# @router.post("/companion/registration/function-call")
-# def determine_companion_registration_function_call(request: DetermineFunctionCallRequest):
-#     result_str = visitor_logic.determine_companion_registration_function_call(request.text)
-#
-#     try:
-#         result = json.loads(result_str)
-#     except json.JSONDecodeError:
-#         raise InvalidAIGeneratedJSONError(detail="Failed to parse JSON from function result.")
-#
-#     valid, errors = validate_json(result, companion_register_schema)
-#     if not valid:
-#         raise InvalidAIGeneratedJSONError(detail=", ".join(errors))
-#
-#     return result
-
 @router.post("/registration/function-call")
 def determine_registration_function_call(request: DetermineFunctionCallRequestOld):
     department_names_list = request.departmentNames
