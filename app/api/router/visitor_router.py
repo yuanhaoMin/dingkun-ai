cfe6f6012645402da869925d1edaa81fbcb95c28
aa <240008537@qq.com>
2023-08-30 17:25:03 +0800

    完善登记

--- app/api/router/visitor_router.py
+++ app/api/router/visitor_router.py
@@ -1,12 +1,9 @@
 import json
 import logging
 from fastapi import APIRouter
-from app.util.chinese_phonetic_util import PyEditDistance
-from app.api.error.json_errors import InvalidAIGeneratedJSONError
-from app.constant.jsonschema.visitor_json_schema import visitor_register_schema, companion_register_schema
 from app.logic import visitor_logic
 from app.model.schema.visitor_schema import DetermineFunctionCallRequest, DetermineFunctionCallRequestSmart
-from app.util.data_validator import validate_json
+
 
 router = APIRouter(
     prefix="/visitor",
@@ -47,22 +44,6 @@ def determine_registration_function_call(request: DetermineFunctionCallRequestSm
     )
 
     return result_str
-
-
-@router.post("/companion/registration/function-call")
-def determine_companion_registration_function_call(request: DetermineFunctionCallRequest):
-    result_str = visitor_logic.determine_companion_registration_function_call(request.text)
-
-    try:
-        result = json.loads(result_str)
-    except json.JSONDecodeError:
-        raise InvalidAIGeneratedJSONError(detail="Failed to parse JSON from function result.")
-
-    valid, errors = validate_json(result, companion_register_schema)
-    if not valid:
-        raise InvalidAIGeneratedJSONError(detail=", ".join(errors))
-
-    return result
 
 
 def handle_department(department, department_names, department_persons_dict):
