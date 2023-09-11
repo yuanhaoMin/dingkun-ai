b919c537803493af8086e67d8c537618b7d24eb0
aa <240008537@qq.com>
2023-09-11 09:46:30 +0800

    解决循环依赖

--- app/db/session/mysql_db.py
+++ app/db/session/mysql_db.py
@@ -1,44 +1,57 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, MetaData, text
-
 from app.api.dependency.database import table_schemas_instance
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://qkwl:qkwl2018@139.9.82.225:3306/uwb_test"
+load_dotenv()
+# Obtain database URL from environment variables
+SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = None
+SessionLocal = None
+
+if SQLALCHEMY_DATABASE_URL:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 metadata = MetaData()
 
 
-# # 存储以_log结尾的表结构
-# log_tables = {}
-
-
 def reflect_log_tables():
-    # 清空元数据中的现有表
-    metadata.clear()
+    # Check if the engine exists
+    if not engine:
+        print("Database not configured. Skipping log table reflection.")
+        return
 
-    # 使用数据库连接获取所有表名
-    with engine.connect() as connection:
-        raw_table_names = connection.execute(text("SHOW TABLES")).fetchall()
-        table_names = [table[0] for table in raw_table_names]
+    try:
+        # 清空元数据中的现有表
+        metadata.clear()
 
-    # 过滤出以 `_log` 结尾的表名
-    log_table_names = [name for name in table_names if name.endswith("_log")]
+        # 使用数据库连接获取所有表名
+        with engine.connect() as connection:
+            raw_table_names = connection.execute(text("SHOW TABLES")).fetchall()
+            table_names = [table[0] for table in raw_table_names]
 
-    # 只反射以 `_log` 结尾的表
-    metadata.reflect(engine, only=log_table_names)
+        # 过滤出以 `_log` 结尾的表名
+        log_table_names = [name for name in table_names if name.endswith("_log")]
 
-    log_tables = metadata.tables
+        # 只反射以 `_log` 结尾的表
+        metadata.reflect(engine, only=log_table_names)
 
-    table_strings = []
-    for table_name, table in log_tables.items():
-        columns_str_list = [
-            f"{column.name}: {column.type.compile(engine.dialect)}"
-            for column in table.columns
-        ]
-        table_string = f"Table: {table_name}\n" + "\n".join(columns_str_list)
-        table_strings.append(table_string)
+        log_tables = metadata.tables
 
-    table_schemas_instance.set_schemas("\n\n".join(table_strings))
+        table_strings = []
+        for table_name, table in log_tables.items():
+            columns_str_list = [
+                f"{column.name}: {column.type.compile(engine.dialect)}"
+                for column in table.columns
+            ]
+            table_string = f"Table: {table_name}\n" + "\n".join(columns_str_list)
+            table_strings.append(table_string)
+
+        table_schemas_instance.set_schemas("\n\n".join(table_strings))
+
+    except Exception as e:
+        print(f"Error while reflecting log tables: {e}")
