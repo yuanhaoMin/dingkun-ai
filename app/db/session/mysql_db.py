d66f050ed99a37d2257413f810ec74225967cb9b
Yuanhao Min <min.yuanhao.1997.11.12@outlook.com>
2023-09-20 10:03:01 +0200

    refactor looping to api

--- app/db/session/mysql_db.py
+++ app/db/session/mysql_db.py
@@ -1,56 +1,49 @@
 import os
-
-from dotenv import load_dotenv
+from fastapi import HTTPException
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, MetaData, text
 from app.api.dependency.database import table_schemas_instance
 
-
-# Obtain database URL from environment variables
 SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL")
 
-engine = None
-SessionLocal = None
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("SQLALCHEMY_DATABASE_URL not set in environment variables!")
 
-if SQLALCHEMY_DATABASE_URL:
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 metadata = MetaData()
 
 
+def get_all_table_names():
+    with engine.connect() as connection:
+        raw_table_names = connection.execute(text("SHOW TABLES")).fetchall()
+    return [table[0] for table in raw_table_names]
+
+
+def filter_log_tables(table_names):
+    return [name for name in table_names if name.endswith("_log")]
+
+
+def format_log_table_schemas(log_tables):
+    table_strings = []
+    for table_name, table in log_tables.items():
+        columns_str_list = [
+            f"{column.name}: {column.type.compile(engine.dialect)}"
+            for column in table.columns
+        ]
+        table_string = f"Table: {table_name}\n" + "\n".join(columns_str_list)
+        table_strings.append(table_string)
+    return "\n\n".join(table_strings)
+
+
 def reflect_log_tables():
-    # Check if the engine exists
-    if not engine:
-        return
-
     try:
-        # 清空元数据中的现有表
         metadata.clear()
-
-        # 使用数据库连接获取所有表名
-        with engine.connect() as connection:
-            raw_table_names = connection.execute(text("SHOW TABLES")).fetchall()
-            table_names = [table[0] for table in raw_table_names]
-
-        # 过滤出以 `_log` 结尾的表名
-        log_table_names = [name for name in table_names if name.endswith("_log")]
-
-        # 只反射以 `_log` 结尾的表
+        all_table_names = get_all_table_names()
+        log_table_names = filter_log_tables(all_table_names)
         metadata.reflect(engine, only=log_table_names)
-
         log_tables = metadata.tables
-
-        table_strings = []
-        for table_name, table in log_tables.items():
-            columns_str_list = [
-                f"{column.name}: {column.type.compile(engine.dialect)}"
-                for column in table.columns
-            ]
-            table_string = f"Table: {table_name}\n" + "\n".join(columns_str_list)
-            table_strings.append(table_string)
-
-        table_schemas_instance.set_schemas("\n\n".join(table_strings))
-
+        formatted_schemas = format_log_table_schemas(log_tables)
+        table_schemas_instance.set_schemas(formatted_schemas)
     except Exception as e:
-        print(f"Error while reflecting log tables: {e}")
+        raise HTTPException(status_code=500, detail=f"Error while reflecting log tables: {e}")
--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 # gunicorn -w 4 -k uvicorn.workers.UvicornWorker app.main:app --timeout 600
 # uvicorn app.main:app --reload
-import schedule
-import time
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from starlette.background import BackgroundTasks
 
 from app.api.error.json_errors import InvalidAIGeneratedJSONError, invalid_ai_generated_json_error_handler
 from app.api.router import visitor_router, data_report_router, helper_router
@@ -35,19 +32,6 @@ def health_check():
     return "Server is up!"
 
 
-def run_schedule():
-    """执行schedule的任务"""
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-
-@app.on_event("startup")
-async def startup_event():
-    # 在启动时首先进行表的反射
-    reflect_log_tables()
-    # 添加周期性任务
-    schedule.every().day.at("04:00").do(reflect_log_tables)
-    # 启动后台任务
-    task = BackgroundTasks()
-    task.add_task(run_schedule)
+@app.get("/reload_mysql_schema")
+def reload_mysql_schema():
+    return reflect_log_tables()
